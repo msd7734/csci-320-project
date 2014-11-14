88d7e010f41d1b9cfbff314afe94658297cb1986
Matt Dennis <msd7734@gmail.com>
2014-11-14 01:33:12 -0500

    Add config encryption lib, encrypt config, add config edit mode

--- SteamFriendExplorer/src/csci320/SteamFriendExplorer.java
+++ SteamFriendExplorer/src/csci320/SteamFriendExplorer.java
@@ -4,18 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.stream.*;
 
 public class SteamFriendExplorer {
 	
 	public static void main(String[] args) {
+		int pathArg = -1;
+		int decryptArg = -1;
+		int modConfigArg = -1;
 		String cfgPathStr = "config";
-		if (args.length > 1) {
+		boolean decrypt = true;
+		
+		//process args
+		for (int i = 0; i < args.length; ++i) {
+			switch(args[i]) {
+				case "-p":
+				case "-path":
+					pathArg = i + 1;
+					break;
+				case "-d":
+				case "-nodecrypt":
+					decryptArg = i;
+					break;
+				case "-c":
+				case "-configMod":
+					modConfigArg = i;
+					break;
+			}
+		}
+		
+		if (pathArg > -1) {
 			try {
-				cfgPathStr = new URI(args[0]).resolve("config").getPath();
+				cfgPathStr = new URI(args[pathArg]).resolve("config").getPath();
 			}
 			catch (URISyntaxException use) {
 				System.out.println("Config path was malformed.");
@@ -23,49 +48,87 @@ public class SteamFriendExplorer {
 			}
 		}
 		
-		BufferedReader cfg;
+		if (decryptArg > -1) {
+			decrypt = false;
+		}
+		
+		if (modConfigArg > -1) {
+			System.out.println("The program will now run in config editing mode.");
+			SteamExplorerConfigFactory cfgFact = new SteamExplorerConfigFactory(cfgPathStr, decrypt);
+			Config cfg = cfgFact.getConfig();
+			Scanner sc = new Scanner(System.in);
+			try {
+				cfg.read();
+				boolean done = false;
+				while (!done) {
+					Set<String> props = cfg.getPropertySet();
+					Iterator<String> it = props.iterator();
+					while (it.hasNext()) {
+						String k = it.next();
+						String v = cfg.getPropertyVal(k);
+						System.out.println(k + " : " + v);
+					}
+					System.out.println("\nEnter a property name to modify it or create it if it doesn't exist,"
+							+ " prepend \'del\' and a space before a property name to permanently delete a property,"
+							+ " or enter \'quit\' to quit the program and save changes.");
+					String p = sc.nextLine();
+					
+					if (p.split(" ")[0].toLowerCase().equals("del")) {
+						cfg.removeProperty(p.split(" ")[1]);
+					}
+					else if (!p.toLowerCase().equals("quit")) {
+						System.out.println("Enter the new value for the property.");
+						String v = sc.nextLine();
+						//this takes advantage of the fact that the config is backed by a HashMap
+						//HashMaps do dynamic create/update so this looks kind of dumb but it works so whatever
+						cfg.setPropertyVal(p.replaceAll("\\s", ""), v);
+					}
+					else {
+						cfg.save();
+						sc.close();
+						return;
+					}
+				}
+			}
+			catch (FileNotFoundException fnfe) {
+				System.out.println("No \"config\" file was found in the current directory."
+						+ "Supply a path argument to the directory where \"config\" can be found.");
+				return;
+			}
+			catch (IOException io) {
+				io.printStackTrace();
+				System.out.print(io.getMessage());
+				return;
+			}
+			finally {
+				sc.close();
+			}
+		}
+
+		SteamExplorerConfigFactory cfgFact = new SteamExplorerConfigFactory(cfgPathStr, decrypt);
+		Config cfg = cfgFact.getConfig();
+		
 		String apiKey = "NO API KEY";
 		String rootUserId = "NO ROOT USER";
 		int maxNodes = 1;
 		
 		try {
-			cfg = new BufferedReader(new FileReader(cfgPathStr));
-			while (cfg.ready()) {
-				String line = cfg.readLine().replaceAll(" ", "");
-				if (line.length() > 0 && !line.substring(0,1).equals(";")) {
-					String[] entry = line.split("=");
-					switch (entry[0]) {
-						case "apikey":
-							apiKey = entry[1];
-							break;
-						case "rootsteamid":
-							rootUserId = entry[1];
-							break;
-						case "maxnodes":
-							try {
-								maxNodes = Integer.parseInt(entry[1]);
-							} catch (NumberFormatException nfe) {
-								System.out.println("Bad maxnodes value \"" + entry[1]
-										+ "\". Defaulting to 1.");
-							}
-							break;
-						default:
-							System.out.println("Unexpected config entry: "
-									+ Arrays.stream(entry).collect(Collectors.joining("=")));
-							break;
-					}
+			cfg.read();
+			if (cfg.getPropertyVal("apikey") != null)
+				apiKey = cfg.getPropertyVal("apikey");
+			if (cfg.getPropertyVal("rootsteamid") != null)
+				rootUserId = cfg.getPropertyVal("rootsteamid");
+			if (cfg.getPropertyVal("maxnodes") != null) {
+				try {
+					maxNodes = Integer.parseInt(cfg.getPropertyVal("maxnodes"));
+				} catch (NumberFormatException nfe) {
+					System.out.println("The value \"maxnodes\" in the config was malformed (not an integer).");
 				}
 			}
-			
-			System.out.println(apiKey);
-			System.out.println(rootUserId);
-			System.out.println(maxNodes);
-			
-			cfg.close();
 		}
 		catch (FileNotFoundException fnfe) {
 			System.out.println("No \"config\" file was found in the current directory."
-					+ "Try supplying a path as an argument instead.");
+					+ "Supply a path argument to the directory where \"config\" can be found.");
 			return;
 		}
 		catch (IOException io) {
