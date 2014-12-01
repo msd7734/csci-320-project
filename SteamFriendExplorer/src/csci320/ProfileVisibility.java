973fcc95e52d57001dcf1e159e152fc7c13a9614
Matt Dennis <msd7734@gmail.com>
2014-12-01 12:56:26 -0500

    Add clarification comment in ProfileVisibility

--- SteamFriendExplorer/src/csci320/ProfileVisibility.java
+++ SteamFriendExplorer/src/csci320/ProfileVisibility.java
@@ -1,6 +1,20 @@
 package csci320;
 
 public enum ProfileVisibility {
+	
+	/**
+	 * Explanation of this class via https://developer.valvesoftware.com/wiki/Steam_Web_API#GetPlayerSummaries_.28v0001.29
+	 * 
+	 * 'communityvisibilitystate'
+	 * This represents whether the profile is visible or not, and if it is visible,
+	 * why you are allowed to see it. Note that because this WebAPI does not use authentication,
+	 * there are only two possible values returned: 1 - the profile is not visible to you
+	 * (Private, Friends Only, etc), 3 - the profile is "Public", and the data is visible.
+	 * Mike Blaszczak's post on Steam forums says, "The community visibility state this API returns
+	 * is different than the privacy state. It's the effective visibility state from the account making
+	 * the request to the account being viewed given the requesting account's relationship to the viewed account."
+	 */
+	
 	hidden(1),
 	visible(3);
 	
